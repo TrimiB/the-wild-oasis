c8ab47360ae9cdd2faa11adec9c43100c98fe6e3
Kushtrim <bujupikushtrim@outlook.com>
2024-09-15 22:03:12 +0200

    [installing] and initalizing supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://ixwbphutnwjiiuktwtgl.supabase.co';
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4d2JwaHV0bndqaWl1a3R3dGdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYzNDE5ODgsImV4cCI6MjA0MTkxNzk4OH0.KQTMoGXhKtDaJIrzt2l0WT1uplO_CHPyEWXjvgBkDhE`;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
