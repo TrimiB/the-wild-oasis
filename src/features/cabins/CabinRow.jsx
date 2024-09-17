88d7492dfe862a5b979a8235b816b4bf76bed0da
Kushtrim <bujupikushtrim@outlook.com>
2024-09-17 21:31:42 +0200

    [refactor] using mutations to delete cabin

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import { formatCurrency } from '../../utils/helpers';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { deleteCabin } from '../../services/apiCabins';
 
 const TableRow = styled.div`
   display: grid;
@@ -41,7 +43,18 @@ const Discount = styled.div`
 `;
 
 function CabinRow({ cabin }) {
-  const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
+  const { id: cabinID, name, maxCapacity, regularPrice, discount, image } = cabin;
+
+  const queryClient = useQueryClient();
+
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: (id) => deleteCabin(id),
+    onSuccess: () => {
+      alert(`Cabin: ${name}, successfully deleted`);
+      queryClient.invalidateQueries({ queryKey: ['cabins'] });
+    },
+    onError: (err) => alert(err.message),
+  });
 
   return (
     <TableRow role='row'>
@@ -50,7 +63,9 @@ function CabinRow({ cabin }) {
       <div>Fits up to {maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
-      <button>Delete</button>
+      <button disabled={isDeleting} onClick={() => mutate(cabinID)}>
+        Delete
+      </button>
     </TableRow>
   );
 }
