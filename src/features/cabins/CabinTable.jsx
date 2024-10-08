ee065ac6c0f8dbe17261886727bad2f6cbf8086e
Kushtrim <bujupikushtrim@outlook.com>
2024-10-08 22:34:52 +0200

    [refactor] Sorting cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -11,6 +11,7 @@ function CabinTable() {
 
   if (isLoading) return <Spinner />;
 
+  // 1. FILTER
   const filterValue = searchParams.get('discount') || 'all';
 
   const filteredCabins = cabins.filter((cabin) => {
@@ -19,6 +20,16 @@ function CabinTable() {
     if (filterValue === 'no-discount') return cabin.discount === 0;
 
     return cabins;
+  });
+
+  // 2. SORT
+  const sortBy = searchParams.get('sortBy') || 'startDate-asc';
+  const [field, direction] = sortBy.split('-');
+  const modifier = direction === 'asc' ? 1 : -1;
+  const sortedCabins = filteredCabins.sort((a, b) => {
+    const aValue = a[field];
+    const bValue = b[field];
+    return (aValue - bValue) * modifier;
   });
 
   return (
@@ -34,7 +45,7 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
