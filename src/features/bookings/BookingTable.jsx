39215e3ee14371fe94c058cfadea6ef8153a5b70
Kushtrim <bujupikushtrim@outlook.com>
2024-10-11 22:24:17 +0200

    [refactor] using count

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -9,7 +9,7 @@ import Spinner from '../../ui/Spinner';
 import Pagination from '../../ui/Pagination';
 
 function BookingTable() {
-  const { bookings, isLoading } = useBookings();
+  const { bookings, isLoading, count } = useBookings();
 
   if (isLoading) return <Spinner />;
 
