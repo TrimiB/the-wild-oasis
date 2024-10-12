04cb9c11f450dee92ceaa50892fdb1481d85ddc3
Kushtrim <bujupikushtrim@outlook.com>
2024-10-12 21:10:22 +0200

    [fromat & refactor] added new route '/bookings/bookingId'

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import Account from './pages/Account';
 import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './ui/AppLayout';
+import Booking from './pages/Booking';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -35,16 +36,52 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route element={<AppLayout />}>
-              <Route index element={<Navigate replace to='dashboard' />} />
-              <Route path='/dashboard' element={<Dashboard />} />
-              <Route path='bookings' element={<Bookings />} />
-              <Route path='cabins' element={<Cabins />} />
-              <Route path='users' element={<Users />} />
-              <Route path='settings' element={<Settings />} />
-              <Route path='account' element={<Account />} />
+              <Route
+                index
+                element={
+                  <Navigate
+                    replace
+                    to='dashboard'
+                  />
+                }
+              />
+              <Route
+                path='/dashboard'
+                element={<Dashboard />}
+              />
+              <Route
+                path='bookings'
+                element={<Bookings />}
+              />
+              <Route
+                path='bookings/:bookingId'
+                element={<Booking />}
+              />
+              <Route
+                path='cabins'
+                element={<Cabins />}
+              />
+              <Route
+                path='users'
+                element={<Users />}
+              />
+              <Route
+                path='settings'
+                element={<Settings />}
+              />
+              <Route
+                path='account'
+                element={<Account />}
+              />
             </Route>
-            <Route path='login' element={<Login />} />
-            <Route path='*' element={<PageNotFound />} />
+            <Route
+              path='login'
+              element={<Login />}
+            />
+            <Route
+              path='*'
+              element={<PageNotFound />}
+            />
           </Routes>
         </BrowserRouter>
 
