df895a0e867c7db15f8470f7963e49053a56ee0a
Kushtrim <bujupikushtrim@outlook.com>
2024-10-13 23:00:36 +0200

    [refactor] conditionlly rendering button for checkout. using useChekcout hook

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { format, isToday } from 'date-fns';
 import { useNavigate } from 'react-router-dom';
 
-import { HiArrowDownOnSquare, HiEye } from 'react-icons/hi2';
+import { HiArrowDownOnSquare, HiArrowUpOnSquare, HiEye } from 'react-icons/hi2';
 
 import Tag from '../../ui/Tag';
 import Table from '../../ui/Table';
@@ -10,6 +10,7 @@ import Menus from '../../ui/Menus';
 
 import { formatCurrency } from '../../utils/helpers';
 import { formatDistanceFromNow } from '../../utils/helpers';
+import useCheckout from '../check-in-out/useCheckout';
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -53,6 +54,7 @@ function BookingRow({
   },
 }) {
   const navigate = useNavigate();
+  const { checkout, isCheckingOut } = useCheckout();
 
   const statusToTagName = {
     unconfirmed: 'blue',
@@ -100,6 +102,14 @@ function BookingRow({
               icon={<HiArrowDownOnSquare />}
               onClick={() => navigate(`/checkin/${bookingId}`)}>
               Check in
+            </Menus.Button>
+          )}
+          {status === 'checked-in' && (
+            <Menus.Button
+              icon={<HiArrowUpOnSquare />}
+              onClick={() => checkout(bookingId)}
+              disabled={isCheckingOut}>
+              Check out
             </Menus.Button>
           )}
         </Menus.List>
