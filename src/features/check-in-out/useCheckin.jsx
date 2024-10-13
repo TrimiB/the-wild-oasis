e5385d7719f48d7673ffe2d046f2168afe769c1d
Kushtrim <bujupikushtrim@outlook.com>
2024-10-13 22:32:12 +0200

    [refactor] hook to recieve a second argument then we spread that argument when updating.

--- src/features/check-in-out/useCheckin.jsx
+++ src/features/check-in-out/useCheckin.jsx
@@ -8,17 +8,18 @@ export default function useCheckin() {
   const navigate = useNavigate();
 
   const { mutate: checkin, isLoading: isCheckingIn } = useMutation({
-    mutationFn: (bookingId) =>
+    mutationFn: ({ bookingId, breakfast }) =>
       updateBooking(bookingId, {
         status: 'checked-in',
         isPaid: true,
+        ...breakfast,
       }),
     onSuccess: (data) => {
       toast.success(`Booking #${data.id} successfully checked in`);
       queryClient.invalidateQueries({ active: true });
       navigate('/');
     },
-    onError: (err) => toast.error('There was an error while checking in'),
+    onError: () => toast.error('There was an error while checking in'),
   });
 
   return { checkin, isCheckingIn };
