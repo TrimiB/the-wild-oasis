a71c8035ecd3a5b2c2009267c5070a6e29aa36d5
Kushtrim <bujupikushtrim@outlook.com>
2024-10-14 16:08:16 +0200

    [format & document]

--- src/ui/ConfirmDelete.jsx
+++ src/ui/ConfirmDelete.jsx
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 import Button from './Button';
 import Heading from './Heading';
 
+/**
+ * Styled component for the ConfirmDelete modal.
+ * Provides a consistent layout and styling for the modal content.
+ */
 const StyledConfirmDelete = styled.div`
   width: 40rem;
   display: flex;
@@ -20,20 +24,36 @@ const StyledConfirmDelete = styled.div`
   }
 `;
 
+/**
+ * A component that displays a confirmation modal for deleting a resource.
+ *
+ * @param {object} props - The component props.
+ * @param {string} props.resourceName - The name of the resource being deleted.
+ * @param {function} props.onConfirm - The function to call when the user confirms the deletion.
+ * @param {boolean} props.disabled - Whether the buttons should be disabled.
+ * @param {function} props.onCloseModal - The function to call when the user cancels the deletion. When used with Modal.Window, this function is automatically passed in.
+ * @returns {JSX.Element} - The ConfirmDelete component.
+ */
 function ConfirmDelete({ resourceName, onConfirm, disabled, onCloseModal }) {
   return (
     <StyledConfirmDelete>
       <Heading as='h3'>Delete {resourceName}</Heading>
       <p>
-        Are you sure you want to delete this {resourceName} permanently? This action cannot be
-        undone.
+        Are you sure you want to delete this {resourceName} permanently? This
+        action cannot be undone.
       </p>
 
       <div>
-        <Button variation='secondary' disabled={disabled} onClick={onCloseModal}>
+        <Button
+          variation='secondary'
+          disabled={disabled}
+          onClick={onCloseModal}>
           Cancel
         </Button>
-        <Button variation='danger' disabled={disabled} onClick={onConfirm}>
+        <Button
+          variation='danger'
+          disabled={disabled}
+          onClick={onConfirm}>
           Delete
         </Button>
       </div>
