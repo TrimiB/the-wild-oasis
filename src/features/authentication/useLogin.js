c3bc1b7ac2f010d1d9df4a9a02f1fa6d76d2c437
Kushtrim <bujupikushtrim@outlook.com>
2024-10-14 22:18:47 +0200

    [new] useLogin hook for loging in users. makes use of login endpoint in apiAuth

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,25 @@
+import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+
+import { login as loginApi } from '../../services/apiAuth';
+
+export function useLogin() {
+  const navigate = useNavigate();
+
+  const { mutate: login, isLoading: isLoggingIn } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+
+    onSuccess: (user) => {
+      console.log(user);
+      toast.success('Logged in successfully');
+      navigate('/dashboard');
+    },
+    onError: (err) => {
+      console.log('ERROR', err);
+      toast.error('Provided email or password are incorrect');
+    },
+  });
+
+  return { login, isLoggingIn };
+}
