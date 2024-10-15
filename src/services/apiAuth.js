5a52079c2c338ee93648ad8c36c714a9b1810493
Kushtrim <bujupikushtrim@outlook.com>
2024-10-15 20:52:23 +0200

    [new] API endpoint to get gurrent user. If there is session then we get user

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -12,3 +12,21 @@ export async function login({ email, password }) {
 
   return data;
 }
+
+export async function getCurrentUser() {
+  /**
+   * Retrieves the current user's session from Supabase.
+   * @returns {Promise<{ session: any } | null>} The current user's session, or null if no session is active.
+   */
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
+
+  /**
+   * Retrieves the current user's information from Supabase.
+   * @returns {Promise<{ user: any } | null>} The current user's information, or null if no user is logged in.
+   */
+  const { data, error } = await supabase.auth.getUser();
+  if (error) throw new Error('Error logging in:', error.message);
+
+  return data?.user;
+}
