c1fc91ee56aa80edddc2cf2890c397c248c563a8
Kushtrim <bujupikushtrim@outlook.com>
2024-10-15 21:00:35 +0200

    [refactor] using queryClient.setQueryData to set new query key to the 'user' query

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,4 +1,4 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
@@ -6,12 +6,14 @@ import { login as loginApi } from '../../services/apiAuth';
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading: isLoggingIn } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
 
     onSuccess: (user) => {
       console.log(user);
+      queryClient.setQueryData(['user'], user?.user);
       toast.success('Logged in successfully');
       navigate('/dashboard');
     },
