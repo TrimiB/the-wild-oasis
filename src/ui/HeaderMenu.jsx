9d1ef217921283e45d5c543de933bbe9b95266f9
Kushtrim <bujupikushtrim@outlook.com>
2024-10-18 21:33:30 +0200

    [new] HeaderMenu component

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import Logout from '../features/authentication/Logout';
+import ButtonIcon from './ButtonIcon';
+import { HiOutlineUserCircle } from 'react-icons/hi2';
+import { useNavigate } from 'react-router-dom';
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+  border-bottom: 0.1rem solid var(--color-grey-100);
+`;
+
+function HeaderMenu() {
+  const navigate = useNavigate();
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate('/account')}>
+          <HiOutlineUserCircle />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+}
+
+export default HeaderMenu;
