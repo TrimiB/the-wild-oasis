a8a5bd6cabbe9e523b53116916650fff9e4e3cc7
Kushtrim <bujupikushtrim@outlook.com>
2024-10-23 19:51:13 +0200

    [remove] console.log

--- src/features/check-in-out/TodayItem.jsx
+++ src/features/check-in-out/TodayItem.jsx
@@ -28,8 +28,6 @@ const Guest = styled.div`
 function TodayItem({ activity }) {
   const { id, status, guests, numNights } = activity;
 
-  console.log(activity);
-
   return (
     <StyledTodayItem>
       {status === 'unconfirmed' && <Tag type='green'>Ariving</Tag>}
