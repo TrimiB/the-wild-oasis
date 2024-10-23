729e4746003841b4fafb913e9cd18d1c5db57ace
Kushtrim <bujupikushtrim@outlook.com>
2024-10-23 22:06:28 +0200

    [refactor] checking if there are no bookings then render Empty component

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -18,6 +18,7 @@ import useCheckout from '../check-in-out/useCheckout';
 import { useDeleteBooking } from './useDeleteBooking';
 import ConfirmDelete from '../../ui/ConfirmDelete';
 import Modal from '../../ui/Modal';
+import Empty from '../../ui/Empty';
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -33,6 +34,8 @@ function BookingDetail() {
   const moveBack = useMoveBack();
 
   if (isLoading) return <Spinner />;
+
+  if (!booking) return <Empty resourceName='booking' />;
 
   const { status, id: bookingId } = booking;
 
