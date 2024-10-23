d80505b031dff3acffdaf13390820a96ea2a7f2a
Kushtrim <bujupikushtrim@outlook.com>
2024-10-23 18:40:47 +0200

    [refactor] using the TodayActivity component

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Spinner from '../../ui/Spinner';
 import Stats from './Stats';
 import SalesChart from './SalesChart';
 import DurationChart from './DurationChart';
+import TodayActivity from '../check-in-out/TodayActivity';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -18,7 +19,7 @@ function DashboardLayout() {
   const { isLoading: isLoadingBooking, bookings } = useRecentBookings();
   const {
     isLoading: isLoadingStays,
-    stays,
+
     confirmedStays,
     numDays,
   } = useRecentStays();
@@ -34,7 +35,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
 
       <SalesChart
